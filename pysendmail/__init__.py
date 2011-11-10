9643650f3b76fb72de5eb015bf5b4438d2a1a1bf
Chaz Lever <chazlever@gatech.edu>
2011-11-09 19:21:40 -0500

    Change package to use setuptools from of distutils

--- pysendmail/__init__.py
+++ pysendmail/__init__.py
@@ -11,6 +11,7 @@ e-mail.
 __author__ = 'Chaz Lever'
 __date__ = '11/17/2010'
 
+import argparse
 import base64
 import email
 from email.Utils import COMMASPACE, formatdate
@@ -21,8 +22,9 @@ import sys
 
 
 class SendMail:
-    """SendMail provides methods for sending e-mail."""
-
+    """
+    SendMail provides methods for sending e-mail.
+    """
     def __init__(self):
         self.User = None
         self.Pass = None
@@ -32,7 +34,9 @@ class SendMail:
         self.ConfigFile = os.path.expanduser('~/.pysendmail')
 
     def writeConfig(self):
-        """Write the configuration file containing server credentials."""
+        """
+        Write the configuration file containing server credentials.
+        """
         try:
             with open(self.ConfigFile, 'w') as f:
                 config = (self.User, self.Pass, self.SmtpServer,
@@ -43,7 +47,9 @@ class SendMail:
             sys.exit("ERROR: %s" % str(e))
 
     def readConfig(self):
-        """Read the configuration file containing server credentials."""
+        """
+        Read the configuration file containing server credentials.
+        """
         try:
             with open(self.ConfigFile, 'r') as f:
                 config = base64.b64decode(f.readline().strip())
@@ -57,8 +63,9 @@ class SendMail:
             sys.exit("ERROR: %s" % str(e))
 
     def send(self, to, subject, message, attachments=None):
-        """Send message to mail server over TLS connection"""
-
+        """
+        Send message to mail server over TLS connection.
+        """
         # BUILD THE E-MAIL MESSAGE
         msg = email.MIMEMultipart.MIMEMultipart()
         msg['From'] = self.User
@@ -92,3 +99,89 @@ class SendMail:
             server.close()
         except smtplib.SMTPException, e:
             sys.exit("ERROR: %s" % str(e))
+
+
+def _main():
+    """
+    Parse options and send e-mail.
+    """
+    # PARSE COMMAND LINE PARAMETERS
+    parser = argparse.ArgumentParser(description=' '\
+            .join(__doc__.split('\n')[3:]))
+
+    parser.add_argument('ACTION',
+        action='store',
+        help='Script action to perform {config,send}')
+
+    # E-MAIL SEND PARAMETERS
+    group = parser.add_argument_group("send options")
+    group.add_argument('-t',
+        action='append',
+        dest='TOLIST',
+        default=[],
+        help='Email addresses in the TO: field')
+    group.add_argument('-s',
+        action='store',
+        dest='SUBJECT',
+        default='',
+        help='E-mail subject')
+    group.add_argument('-m',
+        action='store',
+        dest='MESSAGE',
+        default='',
+        help='E-mail message')
+    group.add_argument('-a',
+        action='append',
+        dest='ATTACHMENTS',
+        default=[],
+        help='Email attachments')
+
+     # E-MAIL CONFIG PARAMETERS
+    group = parser.add_argument_group("config options")
+    group.add_argument('--user',
+        action='store',
+        dest='USER',
+        default=None,
+        help='E-mail login for mail server')
+    group.add_argument('--pass',
+        action='store',
+        dest='PASS',
+        default=None,
+        help='E-mail password for mail server')
+    group.add_argument('--server',
+        action='store',
+        dest='SMTP_SERVER',
+        default='smtp.gmail.com',
+        help='Specify e-mail SMTP server')
+    group.add_argument('--port',
+        action='store',
+        dest='SMTP_PORT',
+        default=587,
+        type=int,
+        help='Specify e-mail SMTP port')
+    group.add_argument('--nossl',
+        action='store_false',
+        dest='SSL',
+        default=True,
+        help='Use SSL to connect to SMTP server')
+
+    results = parser.parse_args()
+
+    if 'config' == results.ACTION:
+        sendMail = SendMail()
+        sendMail.User = results.USER
+        sendMail.Pass = results.PASS
+        sendMail.SmtpServer = results.SMTP_SERVER
+        sendMail.SmtpPort = results.SMTP_PORT
+        sendMail.Ssl = results.SSL
+        sendMail.writeConfig()
+    elif 'send' == results.ACTION:
+        sendMail = SendMail()
+        sendMail.readConfig()
+        sendMail.send(results.TOLIST, results.SUBJECT, results.MESSAGE,
+            results.ATTACHMENTS)
+    else:
+        parser.print_help()
+
+if __name__ == '__main__':
+    _main()
--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='pysendmail',
-      version='1.0',
-      description='Python Module/Script for Sending E-mail',
-      author='Chaz Lever',
-      author_email='pysendmail+github@chazlever.com',
-      url='http://www.github.com/chazlever/pysendmail',
-      packages=['pysendmail'],
-      scripts=['scripts/pysendmail'],
-     )
+setup(
+    name='pysendmail',
+    version='1.0',
+    description='Python Module/Script for Sending E-mail',
+    author='Chaz Lever',
+    author_email='pysendmail+github@chazlever.com',
+    url='http://www.github.com/chazlever/pysendmail',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['pysendmail = pysendmail:_main', ],
+    },
+    zip_safe=True,
+)
